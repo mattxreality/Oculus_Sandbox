d70ad31be739e6c39796908609244ec4c6cbca6a
Matt Hoffman <mattxreality@gmail.com>
2019-06-07 19:02:29 -0400

    More haptic work

---
+++ Assets/VibrateIfGrabbed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrateIfGrabbed : MonoBehaviour
+{
+    private OVRGrabbable ovrGrabbable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ovrGrabbable = GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if object is grabbed and if touch controller button is pressed
+        if (ovrGrabbable.isGrabbed)// && OVRInput.GetDown(shootingButton, ovrGrabbable.grabbedBy.GetController()))
+        {
+            // make controller vibrate using an audio file
+            //VibrationManager.singleton.TriggerVibration(shootingAudio,ovrGrabbable.grabbedBy.GetController());
+
+            // make controller vibrate using manual frequency parameters
+            VibrationManager.singleton.TriggerVibrationManual(40, 2, 255, ovrGrabbable.grabbedBy.GetController());
+        }
+    }
+}
